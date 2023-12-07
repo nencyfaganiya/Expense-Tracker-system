7c51b9221f7f8e97dade12ee5891de89313d241f
Nency <nency@Nencys-MacBook-Air.local>
2023-12-07 12:29:24 +0000

    first commit

---
+++ server/config.js
@@ -0,0 +1,6 @@
+export default {
+    JWT_SECRET : "nency",
+    EMAIL: "steve.franecki@ethereal.email", // testing email & password
+    PASSWORD : "sMf46xCzrvdrxvuagc",
+    ATLAS_URI: "mongodb+srv://nencyfaganiya89:S4NSe3BsKzdFSGgs@login.b2jeqts.mongodb.net/"
+}
